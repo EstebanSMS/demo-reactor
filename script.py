997c1914e2d4f1347d507a5aadcaf5be13e18a62
EstebanSMS <129536918+EstebanSMS@users.noreply.github.com>
2023-03-31 11:31:15 -0500

    Add files via upload

---
+++ script.py
@@ -0,0 +1,11 @@
+#x = int(input('entra un numero: '))
+#print(type(x))
+
+#x = 5
+#print('el numero es: '+ str(x))
+
+# calculadora
+
+first_number = int(input('Type the first number: ')) ;\
+second_number = int(input('Type the second number: ')) ;\
+print('The sum is: ', first_number + second_number)
